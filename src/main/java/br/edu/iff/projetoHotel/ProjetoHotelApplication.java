d46c69fd9a9bd4da36f2ef32ae57f18917ceb1ae
mathe <mathe@LAPTOP-L32BHSR6>
2020-08-25 08:30:53 -0300

    Criação do projeto.

---
+++ src/main/java/br/edu/iff/projetoHotel/ProjetoHotelApplication.java
@@ -0,0 +1,13 @@
+package br.edu.iff.projetoHotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoHotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoHotelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/edu/iff/projetoHotel/ProjetoHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.projetoHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
