64eb0c8954db2fa786529f373c9a8a7bce38091a
mathe <mathe@LAPTOP-L32BHSR6>
2020-08-28 08:51:29 -0300

    Efetuados testes de persistência de alguns objetos. Correção do atributo "inicio" da classe Reserva. Correção do queryMethod findReservasEntreDatas -> parâmetro com o nome errado.

--- src/main/java/br/edu/iff/projetoHotel/ProjetoHotelApplication.java
+++ src/main/java/br/edu/iff/projetoHotel/ProjetoHotelApplication.java
@@ -20,7 +20,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ProjetoHotelApplication implements CommandLineRunner {
+public class ProjetoHotelApplication implements CommandLineRunner{
     
     @Autowired
     private ClienteRepository clienteRepo;
@@ -30,18 +30,17 @@ public class ProjetoHotelApplication implements CommandLineRunner {
     private HotelRepository hotelRepo;
     @Autowired
     private ReservaRepository reservaRepo;
-
+    
     public static void main(String[] args) {
         SpringApplication.run(ProjetoHotelApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        
         //Cliente
         Cliente c1 = new Cliente();
         c1.setNome("Matheus");
-        c1.setCpf("589.173.490-72");
+        c1.setCpf("765.620.220-02");
         c1.setEmail("matheus@gmail.com");
         
         Telefone t1 = new Telefone();
@@ -54,38 +53,37 @@ public class ProjetoHotelApplication implements CommandLineRunner {
         Endereco end = new Endereco();
         end.setRua("Rua das flores");
         end.setNumero(123);
-        end.setBairro("Parque da flores");
+        end.setBairro("Parque das flores");
         end.setCidade("Campos");
         end.setCep("28000-000");
         
         c1.setEndereco(end);
-        
         clienteRepo.save(c1);
         
-        // Funcionario
+        //Funcionario
         Funcionario f1 = new Funcionario();
         f1.setNome("João");
         f1.setEmail("joao@gmail.com");
-        f1.setCpf("127.340.690-75");
+        f1.setCpf("325.291.890-05");
+        f1.setEndereco(end);
+        f1.setTelefones(List.of(t1,t2));
         f1.setSetor("Financeiro");
         f1.setSenha("12345678");
-        f1.setTelefones(List.of(t1,t2));
-        f1.setEndereco(end);
         
         funcionarioRepo.save(f1);
         
         //Hotel
         Hotel h1 = new Hotel();
         h1.setNome("Hotel Miraflores");
-        h1.setCnpj("93.722.399/0001-03");
-        h1.setTelefones(List.of(t1,t2));
+        h1.setCnpj("07.589.505/0001-82");
         h1.setEndereco(end);
+        h1.setTelefones(List.of(t1,t2));
         
         Quarto q1 = new Quarto();
         q1.setNumero(101);
         q1.setTipo(TipoQuartoEnum.LUXO);
         q1.setQtdCamaCasal(1);
-        q1.setQtdCamaSolteiro(2);
+        q1.setQtdCamaSolteiro(1);
         Quarto q2 = new Quarto();
         q2.setNumero(102);
         q2.setTipo(TipoQuartoEnum.STANDARD);
@@ -98,18 +96,22 @@ public class ProjetoHotelApplication implements CommandLineRunner {
         
         // Reserva
         Reserva r1 = new Reserva();
-        r1.setDataHora(Calendar.getInstance());
-        Calendar data1 = Calendar.getInstance();
-        Calendar data2 = Calendar.getInstance();
-        data1.set(2020, 10, 10);
-        data2.set(2020, 10, 20);
-        r1.setInico(data1);
-        r1.setTermino(data2);
         r1.setCliente(c1);
         r1.setFuncionario(f1);
         r1.setQuartos(List.of(q1, q2));
         
+        r1.setDataHora(Calendar.getInstance());
+        
+        Calendar inicio = Calendar.getInstance();
+        Calendar termino = Calendar.getInstance();
+        inicio.set(2020, 10, 10);
+        termino.set(2020, 10, 20);
+        
+        r1.setInico(inicio);
+        r1.setTermino(termino);
+        
         reservaRepo.save(r1);
     }
+
 
 }
